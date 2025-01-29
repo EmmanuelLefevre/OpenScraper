3475a89d4939bc12d83d0a47eb48aaf9cbb64c80
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-01-29 08:43:09 +0100

    maj

--- src/application/services/print_message.py
+++ src/application/services/print_message.py
@@ -1,12 +1,27 @@
+from colorama import Fore, Style
+
 class PrintMessage:
   @staticmethod
+  def print_success(message: str):
+    print(f"{Style.BRIGHT}{Fore.GREEN}✅ {message}{Style.RESET_ALL}")
+
+  @staticmethod
   def print_info(message: str):
-    print(f"[INFO]: {message}")
+    print(f"{Style.BRIGHT}{Fore.BLUE}ℹ {message}{Style.RESET_ALL}")
 
   @staticmethod
   def print_warning(message: str):
-    print(f"[WARNING]: {message}")
+    print(f"{Style.BRIGHT}{Fore.MAGENTA}⚠️ {message}{Style.RESET_ALL}")
 
   @staticmethod
   def print_error(message: str):
-    print(f"[ERROR]: {message}")
+    print(f"{Style.BRIGHT}{Fore.RED}💥 {message}{Style.RESET_ALL}")
+
+  @staticmethod
+  def ask_user_input(message: str):
+    print(f"💬 {message} ('fin' pour quitter) : ", end="")
+
+  @staticmethod
+  def ask_user_confirmation(message: str, default: bool = True):
+    options = "(O/n)" if default else "(o/N)"
+    print(f"💬 {message} {options} : ", end="")
