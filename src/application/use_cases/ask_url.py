9ba309671ea0ddb78f8636807a20d2fc47afa073
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-02-05 08:53:24 +0100

    maj

--- src/application/use_cases/ask_url.py
+++ src/application/use_cases/ask_url.py
@@ -3,4 +3,5 @@ from src.application.services.message_printer import MessagePrinter
 class AskUrl:
   @staticmethod
   def execute() -> str:
-    return MessagePrinter.ask_user_input("🏁 Saisir une URL")
+    url = MessagePrinter.ask_user_input("🏁 Saisir une URL")
+    return url.strip()
