a0e568360b69f0278c16bc4e2e9800a1b13e413d
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-02-05 08:25:54 +0100

    maj

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from src.application.use_cases.display_error import DisplayError
 from src.application.use_cases.display_goodbye import DisplayGoodbye
 from src.application.use_cases.display_leave import DisplayLeave
 from src.application.use_cases.display_welcome import DisplayWelcome
@@ -8,7 +9,7 @@ def main():
     DisplayGoodbye.execute()
 
   except Exception as e:
-    print(f"Une erreur est survenue : {e}")
+    DisplayError.execute(str(e))
 
 if __name__ == "__main__":
   try:
