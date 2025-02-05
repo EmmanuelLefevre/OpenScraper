4a93f2106584296a482eabd1a41e1a7b0488c7bd
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-05 21:14:05 +0100

    maj

--- src/application/services/error_handler.py
+++ src/application/services/error_handler.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import traceback
 
 from src.application.use_cases.display_error import DisplayError
 
@@ -7,33 +8,39 @@ class ErrorHandler:
   LOG_DIR = "logs"
   LOG_FILE = os.path.join(LOG_DIR, "app.log")
 
+
   @staticmethod
   def setup_logging():
     if not os.path.exists(ErrorHandler.LOG_DIR):
       os.makedirs(ErrorHandler.LOG_DIR)
 
     logger = logging.getLogger("app_logger")
+    if logger.hasHandlers():
+      return logger
+
     logger.setLevel(logging.ERROR)
 
-    # Handler fichier
     file_handler = logging.FileHandler(ErrorHandler.LOG_FILE)
     file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
 
-    # Handler console
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
 
-    # Ajouter les handlers au logger
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
     return logger
 
-  # Création unique du logger
   logger = setup_logging.__func__()
 
+
   @staticmethod
-  def handle(message: str, log: bool = True):
+  def handle(error: Exception | str, log: bool = True):
+    message = str(error)
+
+    if isinstance(error, Exception):
+      message += f"\n{traceback.format_exc()}"
+
     if log:
       ErrorHandler.logger.error(message)
-    DisplayError.execute(message)
+    DisplayError.execute(str(error))
