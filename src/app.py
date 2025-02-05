57239517b5a64acb0aa7baa7fa494b398d4fc87b
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-05 08:13:38 +0100

    maj

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from src.application.use_cases.display_welcome import DisplayWelcome
+from src.application.use_cases.display_goodbye import DisplayGoodbye
 
 def main():
   try:
     DisplayWelcome.execute()
+    DisplayGoodbye.execute()
 
   except Exception as e:
     print(f"Une erreur est survenue : {e}")
