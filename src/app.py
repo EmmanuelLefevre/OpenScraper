de8aee08c11eb972c5c2b94c92222b7767c81c5c
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-06 08:25:17 +0100

    maj

--- src/app.py
+++ src/app.py
@@ -1,9 +1,8 @@
 from src.application.use_cases.ask_url import AskUrl
-# from src.application.use_cases.display_error import DisplayError
+from src.application.use_cases.display_exception import DisplayException
 from src.application.use_cases.display_goodbye import DisplayGoodbye
 from src.application.use_cases.display_leave import DisplayLeave
 from src.application.use_cases.display_welcome import DisplayWelcome
-from src.application.services.error_handler import ErrorHandler
 
 def main():
   try:
@@ -12,13 +11,10 @@ def main():
     DisplayGoodbye.execute()
 
   except Exception as e:
-    # DisplayError.execute(str(e))
-    ErrorHandler.handle(e)
+    DisplayException.execute(str(e))
 
 if __name__ == "__main__":
   try:
     main()
   except KeyboardInterrupt:
     DisplayLeave.execute()
-  except Exception as e:
-    ErrorHandler.handle(e)
