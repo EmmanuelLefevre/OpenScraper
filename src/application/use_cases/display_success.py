54a8b8bc90a341cadf9e0912a4acf9609efdefae
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-02-08 14:11:10 +0100

    maj

--- src/application/use_cases/display_success.py
+++ src/application/use_cases/display_success.py
@@ -3,5 +3,5 @@ from src.application.services.message_printer import MessagePrinter
 
 class DisplaySuccess:
   @staticmethod
-  def execute(message: str):
+  def execute(message: str) -> None:
     MessagePrinter.print_success(message)
--- src/application/use_cases/display_warning.py
+++ src/application/use_cases/display_warning.py
@@ -3,5 +3,5 @@ from src.application.services.message_printer import MessagePrinter
 
 class DisplayWarning:
   @staticmethod
-  def execute(message: str):
+  def execute(message: str) -> None:
     MessagePrinter.print_warning(f"{message}")
