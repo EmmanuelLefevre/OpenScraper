a2b14438117991bfeff7465397ccf1f4fe1ed364
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-08 14:02:29 +0100

    maj

--- src/application/controllers/app_controller.py
+++ src/application/controllers/app_controller.py
@@ -3,6 +3,7 @@ from src.application.use_cases.ask_url import AskUrl
 from src.application.use_cases.display_exception import DisplayException
 from src.application.use_cases.display_goodbye import DisplayGoodbye
 from src.application.use_cases.display_leave import DisplayLeave
+from src.application.use_cases.display_warning import DisplayWarning
 from src.application.use_cases.retrieve_data import RetrieveData
 from src.application.use_cases.display_welcome import DisplayWelcome
 from src.infrastructure.external_services.api_client import ApiClient
@@ -13,13 +14,14 @@ class AppController:
   def run():
     try:
       DisplayWelcome.execute()
+
       url = AskUrl.execute()
 
       api_client = ApiClient()
       retrieve_data = RetrieveData(api_client)
-
       data = retrieve_data.execute(url)
       print(data)
+
       DisplayGoodbye.execute()
 
     except UserExitException:
