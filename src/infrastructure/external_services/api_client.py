412e986710399f112ab6ac92a82d04733beff735
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-08 12:24:29 +0100

    maj

--- src/infrastructure/external_services/api_client.py
+++ src/infrastructure/external_services/api_client.py
@@ -1,13 +1,13 @@
 import requests
 
-from src.application.use_cases.display_error import DisplayError
+from src.application.use_cases.display_exception import DisplayException
 
 class ApiClient:
   def get_data(url: str) -> dict:
     try:
-      response = requests.get(url)
+      response = requests.get(url, timeout=10)
       response.raise_for_status()
       return response.json()
 
     except requests.exceptions.RequestException as e:
-      DisplayError.execute(str(e))
+      DisplayException.execute(str(e))
