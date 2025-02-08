bf2a911b6cb57f7cbf42113310b03465d6078039
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-08 13:33:18 +0100

    maj

--- src/application/services/message_printer.py
+++ src/application/services/message_printer.py
@@ -6,29 +6,36 @@ class MessagePrinter:
   def print_message(message: str):
     print(f"{Style.BRIGHT}{Fore.BLUE}{message}{Style.RESET_ALL}")
 
+
   @staticmethod
   def print_success(message: str):
     print(f"{Style.BRIGHT}{Fore.GREEN}✅ {message}{Style.RESET_ALL}")
+
 
   @staticmethod
   def print_info(message: str):
     print(f"{Style.BRIGHT}{Fore.BLUE}ℹ {message}{Style.RESET_ALL}")
 
+
   @staticmethod
   def print_warning(message: str):
     print(f"{Style.BRIGHT}{Fore.MAGENTA}⚠️ {message}{Style.RESET_ALL}")
+
 
   @staticmethod
   def print_error(message: str):
     print(f"{Style.BRIGHT}{Fore.RED}💥 {message}{Style.RESET_ALL}")
 
+
   @staticmethod
   def print_exception(message: str):
     print(f"{Style.BRIGHT}{Fore.RED}💣 {message}{Style.RESET_ALL}")
 
+
   @staticmethod
   def ask_user_input(message: str) -> str:
     return input(f"{message} ('fin' pour quitter) : \n")
+
 
   @staticmethod
   def ask_user_confirmation(message: str, default: bool = True):
