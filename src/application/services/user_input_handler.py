a22e8faf904f07f0d395b1539deaa44c0c6dd53b
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-02-08 14:02:37 +0100

    maj

--- src/application/services/user_input_handler.py
+++ src/application/services/user_input_handler.py
@@ -1,12 +1,19 @@
 from src.application.exceptions.user_exit_exception import UserExitException
 from src.application.services.message_printer import MessagePrinter
+from src.application.use_cases.display_warning import DisplayWarning
 
 
 class UserInputHandler:
   @staticmethod
-  def ask_input(message: str) -> str:
-    user_input = MessagePrinter.ask_user_input(message)
+  def ask_input(message: str, allow_empty: bool = False) -> str:
 
-    if user_input.strip().lower() == "fin":
-      raise UserExitException
-    return user_input.strip()
+    while True:
+      user_input = MessagePrinter.ask_user_input(message).strip()
+
+      if user_input.lower() == "fin":
+        raise UserExitException
+
+      if user_input or allow_empty:
+        return user_input
+
+      DisplayWarning.execute("La saisie ne peut être vide! Essayer à nouveau...")
