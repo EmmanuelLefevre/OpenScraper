5cd4e96e6c3c05865983563108f649c4e217cc72
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-02-08 12:24:02 +0100

    maj

--- src/app.py
+++ src/app.py
@@ -1,20 +1,11 @@
-from src.application.use_cases.ask_url import AskUrl
-from src.application.use_cases.display_exception import DisplayException
-from src.application.use_cases.display_goodbye import DisplayGoodbye
-from src.application.use_cases.display_leave import DisplayLeave
-from src.application.use_cases.display_welcome import DisplayWelcome
+from src.application.controllers.app_controller import AppController
 
 def main():
-  try:
-    DisplayWelcome.execute()
-    url = AskUrl.execute()
-    DisplayGoodbye.execute()
-
-  except Exception as e:
-    DisplayException.execute(str(e))
+  AppController.run()
 
 if __name__ == "__main__":
   try:
     main()
+
   except KeyboardInterrupt:
-    DisplayLeave.execute()
+    AppController.handle_exit()
