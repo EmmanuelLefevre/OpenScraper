37ca325ce2bc8828f267e44654bca4b8be3ca6b2
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-08 12:35:00 +0100

    maj

--- src/application/use_cases/retrieve_data.py
+++ src/application/use_cases/retrieve_data.py
@@ -1,5 +1,7 @@
+from src.application.use_cases.display_error import DisplayError
+from src.application.use_cases.display_success import DisplaySuccess
 from src.infrastructure.external_services.api_client import ApiClient
-from src.application.services.message_printer import MessagePrinter
+
 
 class RetrieveData:
   def __init__(self, api_client):
@@ -8,9 +10,9 @@ class RetrieveData:
   def execute(self, url: str):
     try:
       data = ApiClient.get_data(url)
-      MessagePrinter.print_success("Données récupérées...")
+      DisplaySuccess.execute("Données récupérées...")
       return data
 
     except RuntimeError as e:
-      MessagePrinter.print_error(str(e))
+      DisplayError.execute(str(e))
       return None
