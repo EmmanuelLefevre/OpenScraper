9e9f280ca3dfbbe29fabac60df88f5154ec87a27
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-02-09 21:36:10 +0100

    maj

--- src/application/use_cases/display_warning.py
+++ src/application/use_cases/display_warning.py
@@ -4,4 +4,4 @@ from src.application.services.message_printer import MessagePrinter
 class DisplayWarning:
   @staticmethod
   def execute(message: str) -> None:
-    MessagePrinter.print_warning(f"{message}")
+    MessagePrinter.print_warning(message)
