b880cc28dec729384ded00ef0abf5c66f5f235d2
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-02-09 17:32:31 +0100

    maj

--- src/infrastructure/external_services/api_client.py
+++ src/infrastructure/external_services/api_client.py
@@ -1,15 +1,17 @@
 import requests
 
 from src.application.use_cases.display_exception import DisplayException
+from src.domain.services.data_formatter import DataFormatter
 
 
 class ApiClient:
   @staticmethod
-  def get_data(url: str) -> dict | None:
+  def get_data(url: str) -> str | None:
     try:
       response = requests.get(url, timeout=10)
       response.raise_for_status()
-      return response.json()
+
+      return DataFormatter.format_response(response)
 
     except requests.exceptions.RequestException as e:
       DisplayException.execute(str(e))
