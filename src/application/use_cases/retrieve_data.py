2a31ac5703dc6b105a7c6c2800bfec8a6d40f59d
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-02-09 17:32:10 +0100

    maj

--- src/application/use_cases/retrieve_data.py
+++ src/application/use_cases/retrieve_data.py
@@ -1,17 +1,22 @@
 from src.application.use_cases.display_error import DisplayError
 from src.application.use_cases.display_success import DisplaySuccess
 from src.infrastructure.external_services.api_client import ApiClient
+from src.infrastructure.storage.save_file import SaveFile
 
 
 class RetrieveData:
-  def __init__(self, api_client: ApiClient) -> None:
+  def __init__(self, api_client: ApiClient, save_file: SaveFile) -> None:
     self.api_client = api_client
+    self.save_file = save_file
 
 
-  def execute(self, url: str) -> dict | None:
+  def execute(self, url: str) -> str | None:
     try:
       data = self.api_client.get_data(url)
-      DisplaySuccess.execute("Données récupérées...")
+      if data:
+        filename = "output.json" if "{" in data else "output.csv"
+        self.save_file.save(data, filename)
+        DisplaySuccess.execute(f"Données sauvegardées dans {filename}...")
       return data
 
     except RuntimeError as e:
