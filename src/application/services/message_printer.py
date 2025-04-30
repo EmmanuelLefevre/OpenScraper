650f9c44c8fb8fb4b9a9cc05ff313e1d59011324
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-04-30 08:18:01 +0200

    maj

--- src/application/services/message_printer.py
+++ src/application/services/message_printer.py
@@ -3,55 +3,55 @@ from colorama import Fore, Style
 from src.application.use_cases.user_input import UserInput
 
 class Message(ABC):
-    @abstractmethod
-    def display(self) -> None:
-        pass
+  @abstractmethod
+  def display(self) -> None:
+    pass
 
 class ErrorMessage(Message):
-    def __init__(self, message: str):
-        self.message = message
-    def display(self) -> None:
-        print(f"{Style.BRIGHT}{Fore.RED}💥 {self.message}{Style.RESET_ALL}")
+  def __init__(self, message: str):
+    self.message = message
+  def display(self) -> None:
+    print(f"{Style.BRIGHT}{Fore.RED}💥 {self.message}{Style.RESET_ALL}")
 
 class ExceptionMessage(Message):
-    def __init__(self, message: str):
-        self.message = message
-    def display(self) -> None:
-        print(f"{Style.NORMAL}{Fore.RED}💣 {self.message}{Style.RESET_ALL}")
+  def __init__(self, message: str):
+    self.message = message
+  def display(self) -> None:
+    print(f"{Style.NORMAL}{Fore.RED}💣 {self.message}{Style.RESET_ALL}")
 
 class InfoMessage(Message):
-    def __init__(self, message: str):
-        self.message = message
-    def display(self) -> None:
-        print(f"{Style.BRIGHT}{Fore.CYAN}ℹ  {self.message}{Style.RESET_ALL}")
+  def __init__(self, message: str):
+    self.message = message
+  def display(self) -> None:
+    print(f"{Style.BRIGHT}{Fore.CYAN}ℹ  {self.message}{Style.RESET_ALL}")
 
 class SimpleMessage(Message):
-    def __init__(self, message: str):
-        self.message = message
-    def display(self) -> None:
-        print(f"{Style.NORMAL}{Fore.BLUE}{self.message}{Style.RESET_ALL}")
+  def __init__(self, message: str):
+    self.message = message
+  def display(self) -> None:
+    print(f"{Style.NORMAL}{Fore.BLUE}{self.message}{Style.RESET_ALL}")
 
 class SavedFileMessage(Message):
-    def __init__(self, message: str):
-        self.message = message
-    def display(self) -> None:
-        print(f"{Style.BRIGHT}{Fore.GREEN}📄 {self.message}{Style.RESET_ALL}")
+  def __init__(self, message: str):
+    self.message = message
+  def display(self) -> None:
+    print(f"{Style.BRIGHT}{Fore.GREEN}📄 {self.message}{Style.RESET_ALL}")
 
 class SuccessMessage(Message):
-    def __init__(self, message: str):
-        self.message = message
-    def display(self) -> None:
-        print(f"{Style.BRIGHT}{Fore.GREEN}✅ {self.message}{Style.RESET_ALL}")
+  def __init__(self, message: str):
+    self.message = message
+  def display(self) -> None:
+    print(f"{Style.BRIGHT}{Fore.GREEN}✅ {self.message}{Style.RESET_ALL}")
 
 class WarningMessage(Message):
-    def __init__(self, message: str):
-        self.message = message
-    def display(self) -> None:
-        print(f"{Style.BRIGHT}{Fore.MAGENTA}⚠️ {self.message}{Style.RESET_ALL}")
+  def __init__(self, message: str):
+    self.message = message
+  def display(self) -> None:
+    print(f"{Style.BRIGHT}{Fore.MAGENTA}⚠️ {self.message}{Style.RESET_ALL}")
 
 class BackupAbortedMessage(Message):
-    def __init__(self, message: str):
-        self.message = message
-    def display(self) -> None:
-        print(f"{Style.BRIGHT}{Fore.RED}❌ {self.message}{Style.RESET_ALL}")
-        user_input = UserInput.ask_user_input("Votre message ici")
+  def __init__(self, message: str):
+    self.message = message
+  def display(self) -> None:
+    print(f"{Style.BRIGHT}{Fore.RED}❌ {self.message}{Style.RESET_ALL}")
+    user_input = UserInput.ask_user_input("Votre message ici")
